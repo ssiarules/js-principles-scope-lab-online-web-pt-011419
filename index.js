eef5a8309284cc18d1382384434c4349a158e68e
Johnson Joseph <jhonsonj88@gmail.com>
2019-07-30 15:52:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'Johnson';
+
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  return bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "Joseph";
+}
