2937e587f2cd3c6cf4910f69cecf5ca65ed6e068
Johnson Joseph <jhonsonj88@gmail.com>
2019-07-30 15:33:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(customerName) {
+  var customerName = 'bob';
+   var newcustomerName = newcustomerName.toUpperCase():
+}
