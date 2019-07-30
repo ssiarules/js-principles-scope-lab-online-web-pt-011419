dd9450fa3e1969b86a021d5a52f38bd6818c7fa1
Johnson Joseph <jhonsonj88@gmail.com>
2019-07-30 15:44:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Write your solution in this file!
 var customerName = 'bob';
+var bestCustomer = 'not bob';
 
 function upperCaseCustomerName() {
   return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  return bestCustomer
 }
